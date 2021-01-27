0b69d8880ede17a5856a599eaf5b3667313ff40f
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2021-01-27 17:56:15 +0100

    Add files via upload

---
+++ qr_form.vb
@@ -0,0 +1,8 @@
+﻿Public Class qr_form
+    Private Sub qr_form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.Image = Image.FromFile("qr.jpeg")
+    End Sub
+
+    Private Sub qr_form_CLose(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+    End Sub
+End Class
