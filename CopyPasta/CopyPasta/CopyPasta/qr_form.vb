60c8a9c457db01158ead6e7a6566fe6f7896dfae
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2021-01-29 20:08:16 +0100

    Delete CopyPasta directory

--- CopyPasta/CopyPasta/CopyPasta/qr_form.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class qr_form
-    Private Sub qr_form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        PictureBox1.Image = Image.FromFile("qr.jpeg")
-    End Sub
-
-End Class
