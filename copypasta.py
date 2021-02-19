5529d2a4a5daefd8c9433022ffd3e52c3113cf1f
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2021-02-19 13:17:07 +0100

    Add files via upload

---
+++ copypasta.py
@@ -0,0 +1,212 @@
+import socket
+from flask import Flask, render_template, send_from_directory,send_file,request,redirect,flash
+from requests import get
+from os import path, chdir, mkdir,remove
+import PIL.Image as Image
+from io import BytesIO
+import win32clipboard
+from array import array
+from pyperclip import copy
+from time import sleep
+from random import randint
+from platform import system
+from flaskwebgui import FlaskUI
+from image_proc import start_image_proc
+from text_proc import start_text_proc
+
+
+#init flask app and secret key
+app = Flask(__name__)
+ui = FlaskUI(app,port=80)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.secret_key = b"6{#~@873gJHGZ@sfa54ZZEd^\\@#'"
+
+
+
+#check if the necesarry files exists, if not download and/or create them.
+if not path.exists("static/"):
+    mkdir("static")
+    open("static/hist.blue","w")
+    open("static/dates.Blue","w")
+
+
+
+if not path.exists("templates/"):
+    mkdir("templates")
+
+    r = get("https://raw.githubusercontent.com/thaaoblues/copypasta/master/templates/index.html",allow_redirects=True)
+    with open("templates/index.html","wb") as f:
+        f.write(r.content)
+
+    r = get("https://raw.githubusercontent.com/thaaoblues/copypasta/master/templates/scan_preview.html",allow_redirects=True)
+    with open("templates/scan_preview.html","wb") as f:
+        f.write(r.content)
+
+    r = get("https://raw.githubusercontent.com/thaaoblues/copypasta/master/templates/settings.html",allow_redirects=True)
+    with open("templates/settings.html","wb") as f:
+        f.write(r.content)
+
+    r = get("https://raw.githubusercontent.com/thaaoblues/copypasta/master/templates/favicon.ico",allow_redirects=True)
+    with open("templates/favicon.ico","wb") as f:
+        f.write(r.content)
+
+
+
+#specify the folder where the scan are uploaded
+app.config['UPLOAD_FOLDER'] = "static/"
+
+
+
+#necessary to update images (stack overflow)
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
+
+
+#home
+@app.route("/")
+def home():
+    #read history files, convert it to an array and reverse it to have the most recent first
+    with open("static/hist.Blue","r") as f:
+        a = f.read()
+        a = a.split("=")
+        a.reverse()
+
+        with open("static/dates.Blue","r") as f:
+            dates=f.read().split("\n")
+            dates.reverse()
+        
+            #render the html with the history
+            return render_template("index.html",hist = a, len = len(a),dates=dates)
+
+
+
+@app.route("/hist/<i>")
+def history(i):
+    """
+    :params: i is the index of the scan on the history list, given when and user click on a button
+
+    """
+
+    #read scan history file, convert it to an array and get the specified scan
+    with open("static/hist.Blue","r") as f:
+        a = f.read()
+        a = a.split("=")
+        a.reverse()
+    text = a[int(i)]
+
+    #rewrite the scan temporary file with the old scan
+    with open("static/scan.Blue","w") as f:
+        f.write(text)
+
+    #redirect to the usual scan preview
+    return redirect("/scan_preview")
+
+#settings page
+@app.route("/[SETTINGS]")
+def settings():
+
+    return render_template("settings.html")
+
+#image preview when the user send a picture
+@app.route("/image_preview")
+def img_preview():
+    
+    return render_template("img_preview.html")
+
+
+#scan preview
+@app.route("/scan_preview",methods=["GET", "POST"])
+def scan_preview():
+
+    #download file is post request
+    if request.method == 'POST':
+        #get the file title
+        title = request.form.get("title")
+        #send file
+        return send_file('static/scan.Blue',attachment_filename=title+".txt",as_attachment=True)
+
+    else:
+        #read scan temp file, split it by lines and return it to the template
+        with open("static/scan.Blue","r") as f:
+            a = f.read()
+            leng = len(a.split("\n"))
+            return render_template("scan_preview.html",scan = a.replace("/n","<br>"),len=leng)
+        
+
+#processes
+@app.route("/process/<process_id>")
+def process(process_id):
+
+    #download the image received
+    if process_id == "[DOWNLOAD IMG]":
+        return send_file('static/imgscan.jpeg',
+                     attachment_filename='imgscan'+str(randint(0,167645454))+'.jpeg',
+                     as_attachment=True)
+
+    #empty the scan temporary file
+    if process_id == "[CLEAR SCAN]":
+        open("static/scan.Blue","w")
+        with open("static/scan.Blue","r") as f:
+            return render_template("scan_preview.html",scan = f.read().replace("/n","<br>"))
+
+    #copy the scan temporary file to clipboard
+    if process_id == "[COPY SCAN]":
+        with open("static/scan.Blue","r") as f:
+            copy(f.read())
+            flash("Scan copied in your clipboard :D")
+            return render_template("scan_preview.html")
+    
+    #copy an image to the clipboard with a win32 api
+    if process_id == "[COPY IMG]":
+        output = BytesIO()
+        image = Image.open("static/imgscan.jpeg")
+        image.convert('RGB').save(output, 'BMP')
+        data = output.getvalue()[14:]
+        output.close()
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+        win32clipboard.CloseClipboard()
+        
+        return render_template("img_preview.html")
+
+    #empty the history files
+    if process_id == "[DEL HISTORY]":
+        open("static/hist.Blue","w")
+        open("static/dates.Blue","w")
+        flash("Your scan History has been deleted :D")
+        return redirect("/[SETTINGS]")
+
+
+
+if __name__ == "__main__":
+
+    
+    #make sure we are in the right path
+    chdir(path.abspath(__file__).replace("main.py","").replace("main.exe","").replace("copypasta.exe",""))
+    #get the ip (pray that this is the right one and not some virutal machines)
+    ip = str(socket.gethostbyname(socket.gethostname()))
+
+    #create a qr code containing the ip with google chart api
+    r = get("https://chart.googleapis.com/chart?cht=qr&chs=150x150&chl="+ip,allow_redirects=True)
+    
+    
+
+    #write it
+    with open("static/qr.jpeg","wb") as f:
+        f.write(r.content)
+
+
+    start_image_proc()
+    start_text_proc()
+
+    #run flask web server
+    ui.run()
+    
+
--- launcher.py
+++ launcher.py
@@ -1,5 +1,28 @@
 from subprocess import run
-from os import chdir
+from os import chdir,kill
+from getpass import getuser
+import psutil
 
 chdir("main")
-run(["start","main.exe"],shell=True)
+run(["start","copypasta.exe"],shell=True)
+
+
+import win32gui
+def get_window_titles():
+    ret = []
+    def winEnumHandler(hwnd, ctx):
+        if win32gui.IsWindowVisible(hwnd):
+            txt = win32gui.GetWindowText(hwnd)
+            if txt:
+                ret.append((hwnd,txt))
+
+    win32gui.EnumWindows(winEnumHandler, None)
+    return ret
+
+#check if a window have the name copypasta, if not, close the program
+while True:
+    all_titles = get_window_titles()
+    window_starts = lambda title: [(hwnd,full_title) for (hwnd,full_title) in all_titles if full_title.startswith(title)]
+    all_matching_windows = window_starts('CopyPasta')
+    if len(all_matching_windows) < 1:
+        run(["taskkill","copypasta.exe"])
